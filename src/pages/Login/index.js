e64a7ae31fc66bbab8a2d3d9b54890ea5664ec68
prastudillo <prastudillo@up.edu.ph>
2018-06-13 15:45:46 +0800

    Initial login page

---
+++ src/pages/Login/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue/dist/vue';
+import Login from './Login.vue';
+
+new Vue({
+  el: '#login',
+  render: h => h(Login)
+})
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue/dist/vue';
-import App from './App.vue'
+import App from './App.vue';
 import button from '../../components/button';
+
 
 new Vue({
   el: '#app',
