1b0148a97d4f9f3023a06e31992960f6b21298ce
Daimler M. Clavel <dmclavel@up.edu.ph>
2018-06-19 13:06:45 +0800

    Navbar for Profile

---
+++ src/components/mobile/NavbarFooter.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="icon-bar">
+        <a href="#" class="active"><i class="material-icons">home</i></a> 
+        <a href="#"><i class="material-icons">face</i></a> 
+        <a href="#"><i class="material-icons">shopping_cart</i></a>
+        <a href="#"><i class="material-icons">chat_bubble</i></a> 
+        <a href="#"><i class="material-icons toggle-button">settings</i></a> 
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => {
+            return {
+                data: 0
+            };
+        },
+        methods: {
+            
+        }
+    }
+</script>
+
+<style scoped>
+.icon-bar i{
+    font-size: 30px;
+}
+.icon-bar {
+    width: 100%; /* Full-width */
+    background-color: #804174; 
+    overflow: hidden;
+    position: fixed;
+    bottom: 0;
+}
+
+.icon-bar a {
+    float: left; /* Float links side by side */
+    text-align: center; /* Center-align text */
+    width: 20%; /* Equal width (5 icons with 20% width each = 100%) */
+    padding: 12px 0; /* Some top and bottom padding */
+    transition: all 0.3s ease; /* Add transition for hover effects */
+    color: white; /* White text color */
+    font-size: 25px; /* Increased font size */
+}
+
+.icon-bar i:hover {
+    /* background-color: #000;  */
+    color: #FC2D62;
+}
+</style>
---
+++ src/components/mobile/NavbarProfile.vue
@@ -0,0 +1,71 @@
+<template>
+
+    <div class="navbar-profile">
+        <div id="bottle" class="navbar-profile">
+            <span> Bottles </span>
+            <span> {{bottles}} </span>
+        </div>
+        <div id="bar" class="navbar-profile">
+            <span> Bars </span>
+            <span> {{bars}} </span>
+        </div>
+        <div id="following" class="navbar-profile">
+            <span> Following </span>
+            <span> {{following}} </span>
+        </div>
+
+        <div id="follower" class="navbar-profile">
+            <span> Followers </span>
+            <span> {{followers}} </span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => {
+            return {
+                bottles: 15,
+                bars: 15,
+                following: 100,
+                followers: 2500
+            };
+        },
+        methods: {
+            
+        }
+    }
+</script>
+
+<style scoped>
+    .navbar-profile {
+        width: 100%;
+        height: 50px;
+        letter-spacing: 3px;
+        padding-top: 100px;
+        font-family: 'Montserrat', sans-serif;
+        font-weight: bold;
+    }
+    #bottle {
+        display: inline-grid;
+        text-align: center;
+        width: 22%;
+        padding-left: 10px;
+        
+    }
+    #bar {
+        display: inline-grid;
+        text-align: center;
+        width: 18%;
+    }
+    #following {
+        display: inline-grid;
+        text-align: center;
+        width: 27%;
+    }
+    #follower {
+        display: inline-grid;
+        text-align: center;
+        width: 22%;
+    }
+</style>
