e2a57c2cbba81583d6b315885ff897a4cc32124b
jonlowi <jjmatienzo@up.edu.ph>
2018-06-20 14:39:09 +0800

    modified handlers for profile, followers, and following routes

--- profile/index.js
+++ profile/index.js
@@ -11,6 +11,10 @@ router.get('/:username', (req, res, next) => {
     url: `${process.env.CORE_URL}/profile/${req.params.username}`
   });
   ajax.get()
+  // used to get the stringified body of the response
+  // .then( data => {
+  //   return JSON.parse(data.body);
+  // })
   .then( data => {
     // if (req.baseUrl.indexOf('mobile') > -1) res.render('mobile/profile', data.user);
     // else res.render('web/profile', data.user);
@@ -26,30 +30,38 @@ router.get('/:username/following', (req, res, next) => {
     url: `${process.env.CORE_URL}/profile/${req.params.username}`
   });
   ajax.get()
+  // used to get the stringified body of the response
+  // .then( data => {
+  //   return JSON.parse(data.body);
+  // })
   .then( data => {
     // if (req.baseUrl.indexOf('mobile') > -1) res.render('mobile/following', data.user);
     // else res.render('web/following', data.user);
-    res.json(data);
+    res.send(data);
   })
   .catch( error => {
     res.json(error);
   })
 });
 
-// router.get('/:username/followers', (req, res, next) => {
-//   ajax.setOptions({
-//     url: `${process.env.CORE_URL}/profile/${req.params.username}/followers`
-//   });
-//   ajax.get()
-//   .then( data => {
-//     console.log(req.session);
-//     if (req.baseUrl.indexOf('mobile') > -1) res.render('mobile/followers');
-//     else res.render('web/followers');
-//     res.json(data);
-//   })
-//   .catch( error => {
-//     res.json(error);
-//   })
-// });
+router.get('/:username/followers', (req, res, next) => {
+  ajax.setOptions({
+    url: `${process.env.CORE_URL}/profile/${req.params.username}/followers`
+  });
+  ajax.get()
+  // used to get the stringified body of the response
+  // .then( data => {
+  //   return JSON.parse(data.body);
+  // })
+  .then( data => {
+    console.log(typeof data);
+    // if (req.baseUrl.indexOf('mobile') > -1) res.render('mobile/followers', data.user);
+    // else res.render('web/followers', data.user);
+    res.send(data);
+  })
+  .catch( error => {
+    res.json(error);
+  })
+});
 
 module.exports = router;
