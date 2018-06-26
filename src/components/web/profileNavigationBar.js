054e4aa2807a2098541217e1339d1ccea109cf58
noreenrojales <nrrojales@up.edu.ph>
2018-06-26 16:40:31 +0800

    [FRONTEND] update profile navbar component, avatar and details

--- src/components/web/profileNavigationBar.js
+++ src/components/web/profileNavigationBar.js
@@ -18,38 +18,39 @@ let profileNavigationBar = Vue.component('profile-nav', {
         isEditting: Boolean
     },
   template: `
-    <div class="profilenav">
-        <img class="avatar" :src="avatar"/>
-        
-        <ul>
-            <li> <span id="image"> IMAGE </span> </li>
-            <li> <div id="navitem">
-                    <a href="#"> 
-                        Bars <br/> 
-                            <span> {{bars}} </span>
-                    </a> </div> </li>
+        <div>
+                
+                <div class="profilenav">
+                        <img class="avatar" :src="avatar"/>                              
+                        <ul>
+                        <li> <div id="navitem">
+                                <a href="#"> 
+                                        Bars <br/> 
+                                        <span> {{bars}} </span>
+                                </a> </div> </li>
 
-            <li> <div id="navitem">
-                    <a href="#"> Beers <br/> <span> {{beers}} </span>
-                    </a></div></li>
+                        <li> <div id="navitem">
+                                <a href="#"> Beers <br/> <span> {{beers}} </span>
+                                </a></div></li>
 
-            <li> <div id="navitem">
-                    <a href="#"> Followers <br/> 
-                    <span> {{followers}} </span>
-                    </a></div></li>
-            <li> <div id="navitem">
-                    <a href="#"> Following <br/>
-                        <span> {{following}} </span>
-                    </a> </div> </li> 
-            <li> 
-                <button v-if="isEditting === false" id="editprofile" v-on:click="edit" > Edit Profile</button>
-                <div v-else="isEditting === true">
-                    <button id="editprofile" v-on:click="toggleSave" >Save</button>
-                    <button id="editcancel" v-on:click="edit" >Cancel</button>
+                        <li> <div id="navitem">
+                                <a href="#"> Followers <br/> 
+                                <span> {{followers}} </span>
+                                </a></div></li>
+                        <li> <div id="navitem">
+                                <a href="#"> Following <br/>
+                                        <span> {{following}} </span>
+                                </a> </div> </li> 
+                        <li> 
+                                <button v-if="isEditting === false" id="editprofile" v-on:click="edit" > Edit Profile</button>
+                                <div v-else="isEditting === true">
+                                <button id="editprofile" v-on:click="toggleSave" >Save</button>
+                                <button id="editcancel" v-on:click="edit" >Cancel</button>
+                                </div>
+                        </li>
+                        </ul>
                 </div>
-            </li>
-        </ul>
-    </div>
+        </div>
    `
 
 });
