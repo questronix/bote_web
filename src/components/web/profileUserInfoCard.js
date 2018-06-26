71ed4eeef1165bb3fdf299bbe38bcda6f291b568
zyra <pvgrubat@up.edu.ph>
2018-06-26 14:43:28 +0800

    updated profile user info card

--- src/components/web/profileUserInfoCard.js
+++ src/components/web/profileUserInfoCard.js
@@ -2,18 +2,7 @@ import Vue from 'vue/dist/vue';
 import '../../css/profileuserinfocard.css';
 
 let userInfo = Vue.component('user-info', {
-  data: () => {
-    return {
-      name: 'MhizSx Jhemerlyn',
-      username: '@jh3m3rlYn',
-      address: 'Makati City',
-      email: 'sw33tjhemerlyn16@gmail.com',
-      nameEdit: '',
-      usernameEdit: '',
-      addressEdit: '',
-      emailEdit: ''
-    };
-  },
+
   props: {
     isEditting: Boolean,
     save: Boolean,
@@ -28,23 +17,23 @@ let userInfo = Vue.component('user-info', {
             
             <li> <div id="name"> 
                     <span v-if="isEditting === false"> {{user.name}} </span>
-                    <input v-else="isEditting === true" type="text" :placeholder="name" v-model="edituser.nameEdit">
+                    <input v-else="isEditting === true" type="text" :placeholder="user.name" v-model="edituser.nameEdit">
                     
                 </div> </li>
 
             <li> <div id="username">
                     <span v-if="isEditting === false"> {{user.username}} </span>
-                    <input v-else="isEditting === true" type="text" :placeholder="username" v-model="edituser.usernameEdit">
+                    <input v-else="isEditting === true" type="text" :placeholder="user.username" v-model="edituser.usernameEdit">
                 </div></li>
 
             <li> <div id="address">
                     <span v-if="isEditting === false"> {{user.address}} </span>
-                    <input v-else="isEditting === true" type="text" :placeholder="address" v-model="edituser.addressEdit">
+                    <input v-else="isEditting === true" type="text" :placeholder="user.address" v-model="edituser.addressEdit">
                  </div></li>
 
             <li> <div id="email">
                         <span v-if="isEditting === false"> {{user.email}} </span>
-                        <input v-else="isEditting === true" type="text" :placeholder="email" v-model="edituser.emailEdit">
+                        <input v-else="isEditting === true" type="text" :placeholder="user.email" v-model="edituser.emailEdit">
                  </div> </li> 
         </ul>
     </div>
