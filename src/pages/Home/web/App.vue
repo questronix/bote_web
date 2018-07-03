662fa9c8030413fe8028c86c4c5fc309eb0ba2fb
zyra <pvgrubat@up.edu.ph>
2018-07-03 12:30:03 +0800

    updated landing page; added initial footer component;

--- src/pages/Home/web/App.vue
+++ src/pages/Home/web/App.vue
@@ -1,7 +1,17 @@
 <template>
-  <div id="app">
+  <div id="index">
     
     <nav-bar> </nav-bar>
-    <h1>Index</h1>
+    <div id="botelogo">BOTE</div>
+    <div class="whatsOnBote">
+      <button>What's on BOTE?</button>
+      <info-card title="SHARE A DRINK" description="Lorem ipsum sample description data text information."> </info-card>
+      <info-card title="FIND NEAREST BAR" description="Lorem ipsum sample description data text information."> </info-card>
+      <info-card title="INVITE FRIENDS" description="Lorem ipsum sample description data text information."> </info-card>
+      <info-card title="ANOTHER FEATURE" description="Lorem ipsum sample description data text information."> </info-card>
+    </div>
+    <Footer> </Footer>
+    
+    
   </div>
 </template>
