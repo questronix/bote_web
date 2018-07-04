b9591c219480ce53d871de0d1d2deead47a2112c
Ken Crucillo <kendrick004@gmail.com>
2018-07-04 17:12:00 +0800

    add local storage library persistent data using window.localStorage fix Api.js to include headers

--- src/lib/Api.js
+++ src/lib/Api.js
@@ -6,27 +6,31 @@ class Api {
     }
   }
 
-  static get(route) {
-    return this.xhr(route, null, 'GET');
+  static get(route, headers) {
+    return this.xhr(route, headers, null, 'GET');
   }
 
-  static put(route, params) {
-    return this.xhr(route, params, 'PUT')
+  static put(route, headers, params) {
+    return this.xhr(route, headers, params, 'PUT')
   }
 
-  static post(route, params) {
-    return this.xhr(route, params, 'POST')
+  static post(route, headers, params) {
+    return this.xhr(route, headers, params, 'POST')
   }
 
-  static delete(route, params) {
-    return this.xhr(route, params, 'DELETE')
+  static delete(route, headers, params) {
+    return this.xhr(route, headers, params, 'DELETE')
   }
 
-  static xhr(route, params, verb) {
-    const url = `${window.location.origin}/${route}`
+  static xhr(route, headers, params, verb) {
+    const url = `${window.location.origin}${route}`;
     let options = Object.assign({ method: verb }, params ? { body: JSON.stringify(params) } : null );
     options.credentials = 'same-origin';
-    options.headers = Api.headers();
+    options.headers = Object.assign({}, this.headers(), headers);
+    console.log('AJAX CALL TO WEB-SERVER', {
+      url,  
+      options
+    });
     return fetch(url, options).then( resp => {
       let json = resp.json();
       if (!resp.error) {
