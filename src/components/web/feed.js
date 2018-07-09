aa190dae2f528b613dfb2b87bfe0a209003c30db
noreenrojales <nrrojales@up.edu.ph>
2018-07-09 16:47:59 +0800

    [FRONTEND]  Semi-final profile, with working tabs initialization, array feed post.

--- src/components/web/feed.js
+++ src/components/web/feed.js
@@ -4,7 +4,7 @@ import '../../css/feeds.css';
 let feed = Vue.component('feeds', {
     data: () => {
       return {
-        user_icon: "http://webiconspng.com/wp-content/uploads/2016/11/avatar_business_costume_male_man_office_user_icon_403022.png"
+        user_icon: "https://avatars3.githubusercontent.com/u/5698765?s=460&v=4 "
       };
     },
     props:{
@@ -13,12 +13,28 @@ let feed = Vue.component('feeds', {
     },
     template: 
     `
-    <div class="post">
-      <img :src="user_icon" class="user_icon">
-      <span id="name">{{user.name}}</span>
-      <span id="username">{{user.username}}</span>
-      <span>henlo, this is a trial post</span>
-    </div>
+      <div class="col s12 m8 offset-m2 l6 offset-l3" v-for="(post_feed,index) in post_feeds" :key="index">
+        <div class="card-panel grey lighten-5 z-depth-1">
+          <div class="row valign-wrapper">
+            
+            <div class="col s2">
+              <img src="post_feed.img" alt="" class="circle responsive-img"> 
+            </div>
+          
+            <div class="col s11">
+              <span id="feed_name">{{post_feed.username}}</span>
+              <br/>
+              <span class="black-text">
+                {{post_feed.post}}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+
+
+        
     
     `
     
