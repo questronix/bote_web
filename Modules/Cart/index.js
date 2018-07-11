3007d17fd4879635fdc48abe09bf0c033c1c9503
Jyra Mojica <jemojica@up.edu.ph>
2018-07-11 11:36:18 +0800

    [BACKEND] Revised route for cart viewing

--- Modules/Cart/index.js
+++ Modules/Cart/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next)=>{
-  if (req.baseUrl.indexOf('mobile') > -1) res.render('mobile/cart');
-  else res.render('web/cart');
+router.get('/', (req, res, next) => {
+  if(req.headers["x-access-token"]){
+    ajax.setOptions({
+      url: `${process.env.CORE_URL}/users/${req.body.username}`,
+      headers: req.headers
+    }).get()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(error => {
+      res.json(error);
+    });
+  }else{
+    if (req.baseUrl.indexOf('mobile') > -1) res.render('mobile/cart');
+    else res.render('web/cart');
+  }
 });
 
 module.exports = router;
